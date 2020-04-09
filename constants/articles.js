50ff207887237f0491f6725e6ed663adb3b39faa
Geesa-Vihara <geesavihara97@gmail.com>
2020-04-09 14:12:36 +0530

    20200409 01 start

---
+++ constants/articles.js
@@ -0,0 +1,43 @@
+export default [
+  {
+    title: 'Total Confirmed Cases',
+    image: require("../assets/imgs/hospital.jpg"),
+  },
+  {
+    title: 'Active Cases',
+    image: require("../assets/imgs/bed.jpg"),
+  },
+  {
+    title: 'New Cases',
+    image: require("../assets/imgs/ambulance.jpg"),
+  },
+  {
+    title: 'Under investigations',
+    image: require("../assets/imgs/cross.jpg"),
+  },
+
+  {
+    title: 'Recovered',
+    image: require("../assets/imgs/clap.jpg"),
+  },
+  {
+    title: 'Deaths',
+    image: require("../assets/imgs/dead.jpg"),
+  },
+  {
+    title: '$377',
+    image: require("../assets/imgs/saint-laurent.jpg"),
+    subtitle: 'Black Jacket',
+    description:
+      'The structured shoulders and sleek detailing ensure a sharp silhouette. Team it with a silk pocket.',
+    horizontal: true
+  },
+  {
+    title: '$299',
+    image: require("../assets/imgs/saint-laurent1.jpg"),
+    subtitle: 'Saint Laurent',
+    description:
+      'The smooth woven-wool is water resistant to ensure you stay pristine after a long-haul flight.',
+    horizontal: true
+  }
+];
---
+++ screens/homeMap.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { StyleSheet, Dimensions, ScrollView } from "react-native";
+import { Block, theme, Text } from "galio-framework";
+
+import { Card, Button } from "../components";
+import articles from "../constants/articles";
+const { width } = Dimensions.get("screen");
+
+class homeMap extends React.Component {
+  renderMap = () => {
+    return (
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={styles.articles}
+      >
+      </ScrollView>
+    );
+  };
+
+  render() {
+    return (
+      <Block flex center style={styles.homeMap}>
+        {this.renderMap()}
+      </Block>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  homeMap: {
+    width: width
+  },
+  articles: {
+    width: width - theme.SIZES.BASE * 2,
+    paddingVertical: theme.SIZES.BASE,
+    paddingHorizontal: 2,
+    fontFamily: 'montserrat-regular'
+
+  }
+});
+
+export default homeMap;
